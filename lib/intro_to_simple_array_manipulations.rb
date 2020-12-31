806987927744fa9754e0046260f8aedc288ef225
Karris  Black <karris.b@gmail.com>
2020-12-31 00:48:27 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array, string)
+  array.push(string)
+
+end
+
+def using_unshift(array, string)
+  new_car = ["will iam", "tiptop", "pull left", "far right"]
+  new_car.unshift(string)
+  
+end
+
+def using_pop(array)
+  used_car = ["paul"]
+  used_car.pop
+  
+end
+
